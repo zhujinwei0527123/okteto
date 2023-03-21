359e87afe76cbc64a72ca6afcf8a6b0e709ccc94
zhujinwei0527123 <90101420+zhujinwei0527123@users.noreply.github.com>
2023-03-21 14:35:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,19 +19,6 @@ wget https://gitlab.com/Misaka-blog/xray-paas/-/raw/main/mikutap.zip -O /usr/sha
 unzip -o "/usr/share/nginx/mikutap.zip" -d /usr/share/nginx/html
 rm -f /usr/share/nginx/mikutap.zip
 
-echo "{
-  "RefreshToken": "0.AVIAAJ1OBYtWyEu-2ZBCX3dxMzXc1HhGfsZCkCMtOTFEM6W6ACA.AgABAAEAAAD--DLA3VO7QrddgJg7WevrAgDs_wUA9P95Ezfz9CE-Yg-f6gV5m8SxDMYQ8CMohpz00KM5IpOq1dADILjux14q6v_4XjNLyEKuHZICIOVAwlYOZSfZOC8YbPIvutXuIqNJdg7_VEmBWl4rKSQobuRui4-0e88448osolrH6rACiiZuIv4SWF9ZFKJ2MrBQoIf3i9fUA9-5q_ULIpIZCHqPnloi8VuLuWArGeMlu6NRpaxnFzgKMZkdIjgGiXfDHXJoOO03pKpyELkwws5oP5d0M2Jo_LgA6i_T4mVfzjHfgGXgmLgXpLRgExzTJOoL_xAQlmZunnNAoFFz8UnKbR-9Fmy1-IB3egcFTmHKPs9LVaiglRl48LwJPMu57Fhu-YcrvTA1OfQD8b5DHRs0h1lqkuooKEB-atxjmECMzEDBtxWXkhHQ3nsVANRZrLlhN3q_7EZsjD0C9eY94pOv56FdeuJZ_yn_Fl3RQ14onAKYXW_8TTjtRWjIgQrR21qKPqv2xl5hPlQ6bXvca77trlvaEXRl_17rVTc_KlpvHUCDVgpWr69itGaHvqJmIVmd6sHMr1GmjadKjyqmjB8KVuOqDfmUloY2Cz_UYWR1S1AjGiTVEooApL_iVAu3mfVcz188F_h8J_-rokUb0h86tDmANtF9Gk_rq6N19E_w1Znz_P34eMPf9Dakq7oh7QQ1Fgo6AL8lNZMlCUM_iMjVCnn9Ncp3N6oGRB3pKIWxiGatWuLYD9jiX1mQ4XLlznAaMurLMfqsJzJaFmaYqIpwk9GBJLdOskdF5z-FBZVIOEqZqcSAQemEFaQ29n-bNS_3WQLi-Pepnfoc9NAgMOSuKTdTDGY",
-  "RefreshInterval": 1500,
-  "ThreadNum": "8",
-  "BlockSize": "100",
-  "SigleFile": "100",
-  "MainLand": false,
-  "MSAccount": false
-}" > $pwd/auth.json
-wget https://raw.githubusercontent.com/MoeClub/OneList/master/OneDriveUploader/amd64/linux/OneDriveUploader -P /usr/local/bin/
-chmod +x /usr/local/bin/OneDriveUploader
-/usr/local/bin/OneDriveUploader -c $pwd/auth.json -t 2 -b 210 -s /etc/nginx/nginx.conf -r 录屏/太阳城/test
-
 # 伪装 xray 执行文件
 RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
 mv xray ${RELEASE_RANDOMNESS}
